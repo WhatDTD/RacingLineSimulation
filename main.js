25264689e1fd93c2390d827ea159da667d302edb
AlfSchool <alif.sheikh@itiszuccante.edu.it>
2026-05-17 22:22:21 +0200

    added whatdtd icon, github link to repo and button to download simulations in the homepage

--- main.js
+++ main.js
@@ -18,4 +18,36 @@ window.addEventListener('load', () => {
     viewSimulationButton.addEventListener('click', () => {
         location.href = 'viewSimulation';
     });
+
+    const host = window.location.hostname;
+    let root = "";
+    if (host !== 'localhost' && host !== '127.0.0.1') {
+        root = '/RacingLineSimulation';
+    } 
+
+    const whatdtdIcon = document.querySelector('#whatdtdIcon');
+    whatdtdIcon.src = `${root}/assets/WhatDTD.png`
+
+    const github = document.querySelector('#githubIcon');
+    github.src = `${root}/assets/github.webp`
+    github.addEventListener('click', () => {
+        window.open('https://github.com/WhatDTD/RacingLineSimulation/');
+    });
+    github.style.cursor = 'pointer';
+
+    const downloadExample = document.querySelector('#downloadExample');
+    downloadExample.addEventListener('click', () => {
+        const slowSim = document.createElement('a');
+        slowSim.href = `${root}/assets/slow.RLSsim`;
+        slowSim.download = '';
+        slowSim.click();
+
+        //delay for the second one because some browsers would otherwise block it
+        setTimeout(() => {
+            const fastStim = document.createElement('a');
+            fastStim.href = `${root}/assets/fast.RLSsim`;
+            fastStim.download = '';
+            fastStim.click();
+        }, 500);
+    });
 });
